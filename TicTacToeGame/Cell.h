5c213a7718a024ec5fbc5182c83c0ba4e2cd8127
Seth <s_kith@u.pacific.edu>
2020-10-22 15:22:33 -0700

    Updated .gitignore and Included Cell.h

---
+++ TicTacToeGame/Cell.h
@@ -0,0 +1,23 @@
+/*
+
+	Seth Kith
+	Cell.h
+	This file contains the definitions for the Cell class
+
+*/
+
+#include <iostudio>
+using namespace std;
+
+class Cell {
+	
+	public:
+		Cell();
+		void setMark(int);
+		int getMark();
+
+	private:
+		int mark;
+
+
+}
