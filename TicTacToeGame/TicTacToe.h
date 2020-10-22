cd3ecbc943e2cbd148b91f00876d5d4b491b41fc
Seth <s_kith@u.pacific.edu>
2020-10-21 18:49:15 -0700

    TicTaeToe.h pushed

---
+++ TicTacToeGame/TicTacToe.h
@@ -0,0 +1,42 @@
+/*
+
+Seth Kith
+C++
+Tic Tac Toe
+
+*/
+
+class TicTacToe {
+
+	//Public methods
+	public:
+
+		game();
+				
+		int getPlayer1();
+		int getPlayer2();
+		int getCurrentPlayer();
+		void setPlayer();
+	
+		void displayBoard();
+		void makeMove();
+		void checkWin();
+
+		static const getROW();
+		static const getCOL();
+
+	//Private variables and method	
+	private:
+
+		const int ROW = 3;
+		const int COL = 3;
+		const int player1;
+		const int player2;
+		const int playerWin1;
+		const int playerWin2; 
+
+		Cell gameBoard[3][3];
+		Players players[2];
+		int currentPlayer;
+
+}
